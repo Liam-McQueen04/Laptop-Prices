5ff7bfe23e658d935dcd6fd9a29125839a29814c
Liam-McQueen04 <43755425+Liam-McQueen04@users.noreply.github.com>
2018-11-15 14:20:22 +0000

    Add files via upload

---
+++ Laptop price Calculator.py
@@ -0,0 +1,12 @@
+#Liam McQ - Laptop Prices
+print("Laptop Prices")
+Sungsam = input("How much does the Sungsam Laptop Cost?")
+Pear = input("How much does the Pear Laptop Cost?")
+Dill = input("How much does the Dill Laptop Cost?")
+Mandroid = input("How much does the Mandroid Laptop Cost?")
+Lightbook = input("How much does the Lightbook Laptop Cost?")
+
+total = int(Sungsam) + int(Pear) + int(Dill) + int(Mandroid) + int(Lightbook)
+print("The total cost =",total)
+
+
